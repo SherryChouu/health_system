1a95bf8e65495ffc0b9ca750933139e04b8ef6e2
Evelyn <reset920215@gmail.com>
2023-11-22 16:39:17 +0800

    1122

---
+++ 選擇欲健檢項目.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>選擇欲健檢項目</title>
+    </head>
+    <style>
+        @import url('https://fonts.googleapis.com/earlyaccess/cwtexyen.css');    /*圓體*/
+    </style>
+
+    <body>
+        <main>
+        <div class="navbar">
+                <a href=#>
+                    <div class="logo">
+                        <img src="/Users/sherrychou/Desktop/專題素材/logo_hospital.png" alt="醫院Logo">
+                    </div>
+                </a>
+
+            <h1 class= "title"><a href="#p1">仁愛醫院健檢中心</a></h1>
+            <nav>
+                <ul class="flex-nav">
+                    <li><a href="#">健檢類別查詢</a></li>
+                    <li><a href="#">線上預約</a></li>
+                    <li><a href="#">繳費資訊</a></li>
+                    <li><a href="#">聯絡我們</a></li>
+                </ul>
+            </div>
+        </nav>
+        </main>
+
+        <div id="ck">
+        
+        <button type="button1" >尊爵A</button>
+        <button type="button2" >尊爵B</button>
+        <button type="button3" >尊爵C</button>
+        <button type="button4" >菁英A</button>
+        <button type="button5" >菁英B</button>
+        <button type="button6" >菁英C</button>
+        <button type="button7" >卓越A</button>
+        <button type="button8" >卓越B</button>
+        <button type="button9" >卓越C</button>   
+        </div>
+
+    </body>
+
+</html>
