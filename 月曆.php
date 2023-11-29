8b0eda7eea31029ffc819be7597903cbaf95e458
SherryChouu <sherrychou0710@gmail.com>
2023-11-29 13:11:49 +0800

    1129

--- 月曆.php
+++ 月曆.php
@@ -58,16 +58,16 @@
 <body>
 <main>       
             <div class="navbar">
-                    <a href="index.html">
+                    <a href="index.php">
                         <div class="logo">
                             <img src="images/logo_hospital.png" alt="醫院Logo">
                         </div>
                     </a>
 
-                <h1 class= "title"><a href="index.html">仁愛醫院健檢中心</a></h1>
+                <h1 class= "title"><a href="index.php">仁愛醫院健檢中心</a></h1>
         <nav>
                 <ul class="flex-nav">
-                    <li><a href="健檢類別查詢.html">健檢類別查詢</a></li>
+                    <li><a href="健檢類別查詢.php">健檢類別查詢</a></li>
                     <li><a href="線上預約.php">線上預約</a></li>
                     <li><a href="contact.php">繳費資訊</a></li>
                     <li><a href="pay.php">聯絡我們</a></li>
